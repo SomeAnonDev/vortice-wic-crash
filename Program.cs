b1a386c863e505f0e80aac391d311a6bdc6b27c9
Eugene Ivchenko <goldenjoe@outlook.com>
2021-04-13 18:41:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.IO;
+using Vortice.WIC;
+
+using FileStream stream = File.OpenRead("LuLu.png");
+using var wicFactory = new IWICImagingFactory();
+using IWICStream wicStream = wicFactory.CreateStream(stream);
