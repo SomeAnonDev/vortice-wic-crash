45c08cf6bc7d43211e81bfeeba093d730d53449a
Eugene Ivchenko <goldenjoe@outlook.com>
2021-04-15 20:16:33 +0300

    Another crash

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ using Vortice.WIC;
 using FileStream stream = File.OpenRead("LuLu.png");
 using var wicFactory = new IWICImagingFactory();
 using IWICStream wicStream = wicFactory.CreateStream(stream);
+using IWICBitmapDecoder decoder = wicFactory.CreateDecoderFromStream(wicStream);
